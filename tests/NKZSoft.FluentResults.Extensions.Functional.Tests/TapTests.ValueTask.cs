c2dc08a69bc522de08bfa3abc38c88b78fa1e02b
nkz <5499916+nkz-soft@users.noreply.github.com>
2026-03-06 07:47:38 +0300

    feat: add missing mixed async MapAsync/TapAsync overloads (fixes #73) (#78)

--- tests/NKZSoft.FluentResults.Extensions.Functional.Tests/TapTests.ValueTask.cs
+++ tests/NKZSoft.FluentResults.Extensions.Functional.Tests/TapTests.ValueTask.cs
@@ -34,4 +34,37 @@ public sealed class TapTestsValueTask : TapTestsBase
 
         AssertSuccess(output, isSuccess);
     }
+
+    [Theory]
+    [InlineData(true)]
+    [InlineData(false)]
+    public async Task TapExecutesTaskActionOnResultSuccessAndReturnsSelf(bool isSuccess)
+    {
+        var result = ResultExtensions.OkIfAsync(isSuccess, ErrorMessage);
+        var output = await result.TapAsync(TaskActionEmptyAsync);
+
+        AssertSuccess(output, isSuccess);
+    }
+
+    [Theory]
+    [InlineData(true)]
+    [InlineData(false)]
+    public async Task TapTExecutesTaskActionOnResultSuccessAndReturnsSelf(bool isSuccess)
+    {
+        var result = ResultExtensions.OkIfAsync(isSuccess, ErrorMessage, TValue.Value);
+        var output = await result.TapAsync(TaskActionEmptyAsync);
+
+        AssertSuccess(output, isSuccess);
+    }
+
+    [Theory]
+    [InlineData(true)]
+    [InlineData(false)]
+    public async Task TapTExecutesTaskActionTOnResultSuccessAndReturnsSelf(bool isSuccess)
+    {
+        var result = ResultExtensions.OkIfAsync(isSuccess, ErrorMessage, TValue.Value);
+        var output = await result.TapAsync(TaskActionTAsync);
+
+        AssertSuccess(output, isSuccess);
+    }
 }
